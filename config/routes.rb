9bd55418dd89a016799e5fc9092d90fcd9221583
TRIBASUKI <tribasukikurniawan@yahoo.com>
2013-12-12 09:10:01 +0800

    Deploy

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ DemoApp::Application.routes.draw do
 
   resources :users
 
-  # The priority is based upon order of creation: first created -> highest priority.
+  #  The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
